eedf13dc13254d8f7d07b9f67bc3cafe34f43530
MAN2ll <a.n.moiseev89@gmail.com>
2026-05-30 11:05:10 +0300

    Create Color.kt

---
+++ app/src/main/java/com/securevault/ui/theme/Color.kt
@@ -0,0 +1,19 @@
+package com.securevault.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Dark theme colors
+val DarkBg = Color(0xFF0A0E1A)
+val DarkSurface = Color(0xFF121828)
+val DarkPrimary = Color(0xFF6C9EFF)
+val DarkOnPrimary = Color.White
+val DarkOnSurface = Color(0xFFE8EAF6)
+val DarkError = Color(0xFFFF6B6B)
+
+// Light theme colors
+val LightBg = Color(0xFFF8FAFF)
+val LightSurface = Color.White
+val LightPrimary = Color(0xFF3366CC)
+val LightOnPrimary = Color.White
+val LightOnSurface = Color(0xFF1A1F36)
+val LightError = Color(0xFFD32F2F)
