c82d32282ec561c8218a4f5958781fd77cc600eb
MAN2ll <a.n.moiseev89@gmail.com>
2026-05-30 11:31:42 +0300

    Update LockScreen.kt

--- app/src/main/java/com/securevault/ui/screens/LockScreen.kt
+++ app/src/main/java/com/securevault/ui/screens/LockScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LockScreen(
     onUnlocked: () -> Unit,
