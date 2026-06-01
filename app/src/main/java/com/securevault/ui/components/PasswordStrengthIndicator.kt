5ca3efb4fc6887c809af2fdc5d4eb0d5c83e9985
MAN2ll <a.n.moiseev89@gmail.com>
2026-06-01 21:53:48 +0400

    Update PasswordStrengthIndicator.kt

--- app/src/main/java/com/securevault/ui/components/PasswordStrengthIndicator.kt
+++ app/src/main/java/com/securevault/ui/components/PasswordStrengthIndicator.kt
@@ -6,7 +6,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.securevault.utils.PasswordGenerator
 
 @Composable
@@ -33,13 +35,13 @@ fun PasswordStrengthIndicator(
             modifier = Modifier
                 .fillMaxWidth()
                 .height(6.dp)
-                .background(MaterialTheme.colorScheme.surfaceVariant)
+                .background(color = MaterialTheme.colorScheme.surfaceVariant)
         ) {
             Box(
                 modifier = Modifier
                     .fillMaxSize(fillFraction)
                     .height(6.dp)
-                    .background(colorHex)
+                    .background(color = colorHex)
             )
         }
         Text(
