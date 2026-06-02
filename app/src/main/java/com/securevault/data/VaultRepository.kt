8ae28ca0da984f89e85a145e038184510dd854f1
MAN2ll <a.n.moiseev89@gmail.com>
2026-06-02 12:44:06 +0400

    Update VaultRepository.kt

--- app/src/main/java/com/securevault/data/VaultRepository.kt
+++ app/src/main/java/com/securevault/data/VaultRepository.kt
@@ -9,22 +9,10 @@ class VaultRepository @Inject constructor(
     private val dao: EntryDao
 ) {
     val allEntries: Flow<List<Entry>> = dao.getAll()
-    
-    fun getEntriesByProfile(profile: Profile): Flow<List<Entry>> = dao.getByProfile(profile)
-    
+
     suspend fun getById(id: String): Entry? = dao.getById(id)
-    
-    fun search(query: String): Flow<List<Entry>> = dao.search("%$query%")
-    
-    fun getFavorites(): Flow<List<Entry>> = dao.getFavorites()
-    
-    fun getRotatable(): Flow<List<Entry>> = dao.getRotatable()
-    
     suspend fun insert(entry: Entry) = dao.insert(entry)
-    
     suspend fun update(entry: Entry) = dao.update(entry)
-    
     suspend fun delete(entry: Entry) = dao.delete(entry)
-    
-    suspend fun deleteById(id: String) = dao.deleteById(id)
+    suspend fun deleteAll() = dao.deleteAll()
 }
