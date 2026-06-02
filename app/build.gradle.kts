fdb5116c0d9ed5ca6e4c480527b67e67e7e34b11
MAN2ll <a.n.moiseev89@gmail.com>
2026-06-02 12:49:28 +0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,6 +75,7 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.48")
     ksp("com.google.dagger:hilt-compiler:2.48")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
     
     // === ROOM ===
     implementation("androidx.room:room-runtime:2.6.1")
