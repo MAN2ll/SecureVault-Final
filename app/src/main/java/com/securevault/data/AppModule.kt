b6481cbaef338e159db32f92378958ea36754d9c
MAN2ll <a.n.moiseev89@gmail.com>
2026-06-02 12:45:12 +0400

    Create AppModule.kt

---
+++ app/src/main/java/com/securevault/data/AppModule.kt
@@ -0,0 +1,25 @@
+package com.securevault.data
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext context: Context): AppDatabase {
+        return AppDatabase.getDatabase(context)
+    }
+
+    @Provides
+    fun provideEntryDao(database: AppDatabase): EntryDao {
+        return database.entryDao()
+    }
+}
