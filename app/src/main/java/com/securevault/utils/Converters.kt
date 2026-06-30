5389a655f6748373b270385c747f129ae62530d9
MAN2ll <a.n.moiseev89@gmail.com>
2026-06-30 11:15:39 +0400

    Update Converters.kt

--- app/src/main/java/com/securevault/utils/Converters.kt
+++ app/src/main/java/com/securevault/utils/Converters.kt
@@ -1,19 +1,12 @@
 package com.securevault.utils
 
 import androidx.room.TypeConverter
-import com.securevault.data.Profile
 import java.util.*
 
 class Converters {
     @TypeConverter
     fun fromTimestamp(value: Long?): Date? = value?.let { Date(it) }
-    
+
     @TypeConverter
     fun dateToTimestamp(date: Date?): Long? = date?.time
-    
-    @TypeConverter
-    fun fromProfile(profile: Profile): String = profile.name
-    
-    @TypeConverter
-    fun toProfile(value: String): Profile = Profile.valueOf(value)
 }
