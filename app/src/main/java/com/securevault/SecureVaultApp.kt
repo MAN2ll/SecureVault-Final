acb31ebb1f1430a54d27643b59fcf166ad8ee770
MAN2ll <a.n.moiseev89@gmail.com>
2026-06-30 13:50:42 +0400

    Update SecureVaultApp.kt

--- app/src/main/java/com/securevault/SecureVaultApp.kt
+++ app/src/main/java/com/securevault/SecureVaultApp.kt
@@ -1,7 +1,20 @@
 package com.securevault
 
 import android.app.Application
+import com.securevault.utils.AutoLockManager
+import com.securevault.utils.NotificationHelper
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class SecureVaultApp : Application()
+class SecureVaultApp : Application() {
+    
+    override fun onCreate() {
+        super.onCreate()
+        
+        // ✅ Инициализация AutoLockManager
+        AutoLockManager.init(applicationContext)
+        
+        // ✅ Создание канала уведомлений
+        NotificationHelper.createNotificationChannel(applicationContext)
+    }
+}
