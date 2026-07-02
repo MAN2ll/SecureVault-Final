4d2dcd02767ea240270f4b0eb4ec7e038dcdb46f
MAN2ll <a.n.moiseev89@gmail.com>
2026-07-02 16:35:55 +0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    id("com.android.application") apply false
-    id("com.android.library") apply false
-    id("org.jetbrains.kotlin.android") apply false
-    id("com.google.dagger.hilt.android") apply false
+    id("com.android.application") version "8.2.2" apply false
+    id("com.android.library") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    id("com.google.dagger.hilt.android") version "2.48" apply false
 }
 
 tasks.register("clean", Delete::class) {
