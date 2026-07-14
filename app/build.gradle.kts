53b1fb9afd1da48bcf8e6443f709753603fd2e0f
MAN2ll <a.n.moiseev89@gmail.com>
2026-07-14 14:11:37 +0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,6 +75,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
+    implementation("androidx.biometric:biometric:1.2.0-alpha05")
 
     // ===== Navigation =====
     implementation("androidx.navigation:navigation-compose:2.7.7")
