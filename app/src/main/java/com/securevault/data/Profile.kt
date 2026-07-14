800a672d0ef80a0e8c6a70cd25389d2bfc3382ff
MAN2ll <a.n.moiseev89@gmail.com>
2026-07-14 14:12:56 +0400

    Update Profile.kt

--- app/src/main/java/com/securevault/data/Profile.kt
+++ app/src/main/java/com/securevault/data/Profile.kt
@@ -1,13 +1,16 @@
 package com.securevault.data
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.securevault.utils.AccessMode
 
 @Entity(tableName = "profiles")
 data class Profile(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
-    val name: String,
-    val passwordHash: String,
-    val passwordSalt: String, // : соль для хэширования
-    val createdAt: Long = System.currentTimeMillis()
+    @ColumnInfo(name = "name") val name: String,
+    @ColumnInfo(name = "password_hash") val passwordHash: String,
+    @ColumnInfo(name = "password_salt") val passwordSalt: String,
+    @ColumnInfo(name = "created_at") val createdAt: Long = System.currentTimeMillis(),
+    @ColumnInfo(name = "password_access_mode") val passwordAccessMode: String = AccessMode.PIN_REQUIRED.value
 )
