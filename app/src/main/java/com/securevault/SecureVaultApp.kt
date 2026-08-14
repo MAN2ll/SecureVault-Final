b430cf5c9cda5461228fd24a8563492715bf7ccc
MAN2ll <a.n.moiseev89@gmail.com>
2026-08-14 14:06:48 +0400

    Update SecureVaultApp.kt

--- app/src/main/java/com/securevault/SecureVaultApp.kt
+++ app/src/main/java/com/securevault/SecureVaultApp.kt
@@ -1,24 +1,11 @@
 package com.securevault
 
 import android.app.Application
-import com.securevault.security.BruteForceGuard
-import com.securevault.utils.AutoLockManager
-import com.securevault.utils.NotificationHelper
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class SecureVaultApp : Application() {
-    
     override fun onCreate() {
         super.onCreate()
-        
-        //  Инициализация AutoLockManager
-        AutoLockManager.init(applicationContext)
-        
-        //  Создание канала уведомлений
-        NotificationHelper.createNotificationChannel(applicationContext)
-        
-        //  Инициализация BruteForceGuard (ОБЯЗАТЕЛЬНО!)
-        BruteForceGuard.init(applicationContext)
     }
 }
