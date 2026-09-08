b8da1599091a0121ff268643d1a55e617b3b889c
MAN2ll <a.n.moiseev89@gmail.com>
2026-09-08 13:30:36 +0400

    Update ProfileListScreen.kt

--- app/src/main/java/com/securevault/ui/screens/ProfileListScreen.kt
+++ app/src/main/java/com/securevault/ui/screens/ProfileListScreen.kt
@@ -3,6 +3,8 @@
 package com.securevault.ui.screens
 
 import androidx.compose.foundation.clickable
+import com.securevault.utils.AccessResult
+import com.securevault.utils.resolveProfileAccess
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
